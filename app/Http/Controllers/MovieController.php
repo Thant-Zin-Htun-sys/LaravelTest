8f20c5f8604016b85aaafc8bafd615a915c56ff9
Thant Zin Htun <thantzintun608@gmail.com>
2025-04-03 15:10:41 +0630

    CRUD

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -17,4 +17,58 @@ class MovieController extends Controller
         $averageRating = $movie->rating()->avg('rating');
         return view('movies.show', compact('movie','averageRating'));
     }
+
+    public function create()
+    {
+        return view('movies.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'genre' => 'required|string|max:100',
+            'released_date' => 'required|date',
+        ]);
+
+        Movie::create([
+            'name' => $request->name,
+            'genre' => $request->genre,
+            'released_date' => $request->released_date,
+        ]);
+
+        return redirect()->route('movies.index')->with('success', 'Movie added successfully!');
+    }
+
+    public function edit($id)
+    {
+        $movie = Movie::findOrFail($id);
+        return view('movies.edit', compact('movie'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'genre' => 'required|string|max:100',
+            'released_date' => 'required|date',
+        ]);
+
+        $movie = Movie::findOrFail($id);
+        $movie->update([
+            'name' => $request->name,
+            'genre' => $request->genre,
+            'released_date' => $request->released_date,
+        ]);
+
+        return redirect()->route('movies.index')->with('success', 'Movie updated successfully!');
+    }
+
+    public function destroy($id)
+    {
+        $movie = Movie::findOrFail($id);
+        $movie->delete();
+
+        return redirect()->route('movies.index')->with('success', 'Movie deleted successfully!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,9 @@ Route::prefix('movies')->group(function () {
     Route::get('/', [MovieController::class, 'index'])->name('movies.index');
     Route::get('/{id}', [MovieController::class, 'show'])->name('movies.show');
     Route::post('/{movie_id}/rate', [RatingController::class, 'store'])->name('movies.rate');
+    Route::get('/create', [MovieController::class, 'create'])->name('movies.create');
+    Route::get('/', [MovieController::class, 'store'])->name('movie.store');
+    Route::get('/{id}/edit', [MovieController::class, 'edit'])->name('movies.edit');
+    Route::put('/{id}', [MovieController::class, 'update'])->name('movies.update');
+    Route::delete('/{id}', [MovieController::class, 'destroy'])->name('movies.destroy');
 });
